1f12c348c7d97c4e36e3a697ca23aaff11d9829e
Nick Thorpe <nickthorpe71@gmail.com>
2022-06-18 10:06:20 -0500

    add basic server setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,4 +6,21 @@ import config from 'config';
 
 const port = config.get<number>("port");
 const host = config.get<string>("host");
+const corsOrigin = config.get<string>("corsOrigin");
 
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: corsOrigin,
+        credentials: true
+    },
+});
+
+app.get('/', (_, res) => res.send(`Server is up`));
+
+httpServer.listen(port, host, () => {
+    console.log('Server is listening');
+});
